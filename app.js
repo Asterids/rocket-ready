bdef335adc00d92bd9291ee756cc9c535e031352
Ruth Hill <ruthhill.e@gmail.com>
2018-01-20 23:12:08 -0500

    cleaned up app.js 'listen' call

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
 const app = require('./server');
 
-const port = process.env.port || 3000
-
-app.listen(port, function () {
-  console.log(`The server is listening on port ${port}`)
-});
+app.listen(process.env.port || 3000);
